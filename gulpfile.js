089b4be5b98baa4344080d168803c27bea87bcb6
Kosuke Kusano <cocuh.kk@gmail.com>
2015-09-21 00:18:15 +0900

    rotating icon

--- gulpfile.js
+++ gulpfile.js
@@ -10,35 +10,45 @@ var config = require('./webpack.config.js');
 gulp.task('clean', del.bind(null, ['build/*']));
 
 gulp.task('compile:dom', function () {
-    return gulp.src(['src/**/*.{html,svg}']).pipe(gulp.dest('build'));
+  return gulp.src(['src/**/*.{html,svg}']).pipe(gulp.dest('build'));
 });
 
-gulp.task('compile', ['webpack', 'compile:dom']);
+gulp.task('compile:style', function () {
+  return gulp.src(['src/**/*.less'])
+    .pipe($.less({}))
+    .on('error', function() {})
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('copy', function () {
+  return gulp.src(['src/**/*.png']).pipe(gulp.dest('build'));
+});
+
+gulp.task('compile', ['webpack', 'compile:dom', 'compile:style']);
 
 gulp.task("webpack", ["webpack:main"]);
 
 gulp.task("webpack:main", function () {
-    return gulp.src(config.main.entry)
-        .pipe($.webpack(config.main))
-        .pipe(gulp.dest(config.js.dest));
+  return gulp.src(config.main.entry)
+    .pipe($.webpack(config.main))
+    .pipe(gulp.dest(config.js.dest));
 });
 
 gulp.task('default', function () {
-    runSequence('clean', 'compile');
+  runSequence('clean', 'compile', 'copy');
 });
 
 gulp.task('serve', function () {
-    gulp.src('build')
-        .pipe($.webserver({
-            livereload: false,
-            directroyListing: true,
-            open: true
-        }));
+  gulp.src('build')
+    .pipe($.webserver({
+      livereload: false,
+      directoryListing: false,
+      open: true
+    }));
 });
 
-gulp.task('watch', function () {
-    runSequence('clean', 'compile');
-    gulp.watch(['src/**/*'], function () {
-        runSequence('clean', 'compile');
-    });
+gulp.task('watch', ['default'], function () {
+  gulp.watch(['src/**/*'], function () {
+    runSequence('default');
+  });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,28 @@ const dest = './build';
 const src = './src';
 
 module.exports = {
-    js: {
-        src: './src/js/**',
-        dest: './build/js'
+  js: {
+    src: './src/js/**',
+    dest: './build/js'
+  },
+  main: {
+    entry: './src/js/main.jsx',
+    output: {
+      filename: 'main.js'
     },
-    main: {
-        entry: './src/js/main.jsx',
-        output: {
-            filename: 'main.js'
-        },
-        devtool: 'inline-source-map',
-        resolve: {
-            extensions: ['', '.js', '.jsx'],
-            modulesDirectories: ['node_modules', 'bower_components']
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.jsx?$/,
-                    exclude: /(node_modules|bower_components)/,
-                    loader: require.resolve('babel-loader')
-                }
-            ]
+    devtool: 'inline-source-map',
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+      modulesDirectories: ['node_modules', 'bower_components']
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: require.resolve('babel-loader')
         }
+      ]
     }
+  }
 };
